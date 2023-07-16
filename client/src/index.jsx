45927b12d9a5dc4396619f0b17573cc882f7348b
itslewisndungu <itslewisndungu@gmail.com>
2023-07-16 06:54:10 +0300

    Remove nested ProtectedRoutes element Use BrowserRouter instead of normal Router

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,16 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { AuthContextProvider } from "./contexts/authContext";
-import { Router } from "react-router-dom";
+import {AuthContextProvider} from "./contexts/authContext";
+import {BrowserRouter, Router} from "react-router-dom";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <AuthContextProvider>
-      <Router>
-        <App />
-      </Router>
-    </AuthContextProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <AuthContextProvider>
+                <BrowserRouter>
+                    <App/>
+                </BrowserRouter>
+            </AuthContextProvider>
+        </QueryClientProvider>
+    </React.StrictMode>
 );
